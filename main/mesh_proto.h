144ffcef26016f26ba75a9c72a6ffa6a7defa1d6
kennet-one <kennet1one@gmail.com>
2026-07-04 22:01:27 +0200

    pin shared mesh runtime fix

--- main/mesh_proto.h
+++ main/mesh_proto.h
@@ -2,6 +2,6 @@
 
 #include "keemash_mesh_proto.h"
 
-#if KEEMASH_MESH_CORE_VERSION != 0x00030000UL
-#error "firmware requires keemash_mesh_core 0.3.0"
+#if KEEMASH_MESH_CORE_VERSION != 0x00030100UL
+#error "firmware requires keemash_mesh_core 0.3.1"
 #endif
